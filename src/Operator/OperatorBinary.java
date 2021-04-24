39b232d8ae8302eabf152d967745fe9a500a240f
Erlina Rusmani <erlinarusmani21@gmail.com>
2021-04-25 05:04:30 +0700

    operator Binary Assignment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,11 @@
+package Operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai sebelum variable
+    // baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Erlina Rusmani";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+}
